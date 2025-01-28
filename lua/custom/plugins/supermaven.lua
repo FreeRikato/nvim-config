53034bf75511d28afde493fe047641cae94be11a
FreeRikato <aravinthanrc@gmail.com>
2025-01-28 21:58:41 +0530

    sync

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,28 +1,29 @@
--- ~/.config/nvim/lua/custom/plugins/supermaven.lua
+-- supermaven.lua
 
 return {
   'supermaven-inc/supermaven-nvim',
   config = function()
     require('supermaven-nvim').setup {
       keymaps = {
-        -- accept_suggestion = '<C-;>', -- Accept a suggestion
-        accept_suggestion = '<Tab>', -- Accept a suggestion
-        clear_suggestion = '<C-]>', -- Clear the current suggestion
-        accept_word = '<C-j>', -- Accept the next word in the suggestion
+        accept_suggestion = '<Tab>', -- Accept the suggestion
+        clear_suggestion = '<C-]>', -- Clear the suggestion
+        accept_word = '<C-j>', -- Accept the suggestion up to the end of the next word
       },
       ignore_filetypes = {
-        markdown = true,
+        cpp = true, -- Ignore suggestions for C++ files
+        -- Add other filetypes you want to ignore here
       },
       color = {
-        suggestion_color = '#ffffff', -- Set suggestion text color
-        cterm = 244, -- Terminal color for suggestion text
+        suggestion_color = '#ffffff', -- Set the suggestion color
+        cterm = 244, -- Set the cterm color
       },
-      log_level = 'info', -- Log level: "off", "error", "warn", "info", or "debug"
+      log_level = 'info', -- Set the log level to "info"
       disable_inline_completion = false, -- Enable inline completion
-      disable_keymaps = false, -- Use built-in keymaps
-      condition = function()
-        return false -- Always enable Supermaven unless condition returns true
-      end,
+      disable_keymaps = false, -- Enable built-in keymaps
+      condition = function() end,
     }
+
+    -- Optional: Highlight for Supermaven in nvim-cmp
+    vim.api.nvim_set_hl(0, 'CmpItemKindSupermaven', { fg = '#6CC644' })
   end,
 }
